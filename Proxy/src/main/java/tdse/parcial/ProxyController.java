1045ad5a3271bc692d4070584926b82645805e75
unknown <tulio.riano-s@labinfo.is.escuelaing.edu.co>
2026-03-24 10:05:33 -0500

    Añadiendo las urls de las EC2 y más imagenes para el Readme

--- Proxy/src/main/java/tdse/parcial/ProxyController.java
+++ Proxy/src/main/java/tdse/parcial/ProxyController.java
@@ -11,12 +11,21 @@ public class ProxyController {
     
 
     @GetMapping("/proxy")
-    public ProxyBody rellSequence(@RequestParam(value="value") int value) throws IOException{
+    public ProxyBody rellSequence(@RequestParam(value="value") int value){
 
-        String pellSequence = ProxyConnection.startConnection("http://localhost:6500/pellseq?value="+value);
+        try {
+            String pellSequence = ProxyConnection.startConnection("http://ec2-54-211-214-228.compute-1.amazonaws.com:6500/pellseq?value="+value);
+            return new ProxyBody("Secuencia de Pell",value, pellSequence);
+        } catch (IOException e) {
+            System.err.println("The First Instance is Dead, tryinng the second one");
+            try {
+                String pellSequenceInstanceTwo = ProxyConnection.startConnection("http://ec2-13-219-73-154.compute-1.amazonaws.com:6500/pellseq?value="+value);
+                return new ProxyBody("Secuencia de Pell", value, pellSequenceInstanceTwo);
 
-        return new ProxyBody("Secuencia de Pell",value, pellSequence);
-
+            } catch (IOException i) {
+                throw new ProxyException("Both instances are dead");
+            }
+        }
     }
 
 
--- README.md
+++ README.md
@@ -8,3 +8,27 @@ Prueba local Math Service
 Pruba local Proxy redirigiendo a instancia de MathService
 
 ![alt text](image-1.png)
+
+Creando instancias en AWS
+
+![alt text](image-2.png)
+
+Instalando en EC2 java 17
+
+![alt text](image-4.png)
+
+Pasando .class de MathService a EC2
+
+![alt text](image-3.png)
+
+Desplegue funcional en Ec2 de PellSeq
+
+![alt text](image-5.png)
+
+Despliegue funcional de la segunda Ec2 de PellSeq
+
+![alt text](image-6.png)
+
+Probando desde Postman la peticion directamente hacia la instancia
+
+![alt text](image-7.png)
